e3e9271d904b4b2e9602f29210f2547629d7903d
Thomas Gooch <thomas.gooch@mckesson.com>
2021-07-28 14:45:42 -0400

    implemented List and Detail view within Web

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -56,5 +56,17 @@ namespace Mango.Web.Controllers
         {
             return SignOut("Cookies", "oidc");
         }
+        
+        [Authorize]
+        public async Task<IActionResult> Details(int productId)
+        {
+            ProductDto model = new();
+            var response = await _productService.GetProductByIdAsync<ResponseDto>(productId, "");
+            if (response != null && response.IsSuccess)
+            {
+                model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+            }
+            return View(model);
+        }
     }
 }
